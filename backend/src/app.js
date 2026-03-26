6e7a084b93f52561ce4e006cf59db7cf82780739
Aoi-2k <geetasharma2827@gmail.com>
2026-03-26 23:18:09 +0530

    added logger middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 
+const logger = require(".middleware/logger.middleware");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(logger);
 
 // import routes
 const userRoutes = require("./routes/user.routes");
